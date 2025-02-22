f18ebb479a73f476a38a4153f38e35943fd7dba1
Thalysoncristian <thalysoncristian19@gmail.com>
2025-02-22 03:15:40 -0300

    notificação

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,17 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-       (cacheNames => {
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
